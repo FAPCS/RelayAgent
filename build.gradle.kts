999a2362765945bcda9a7c223851ee2026a2b4d9
Lian <cheesetastisch@gmail.com>
2023-01-29 18:06:21 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "me.fapcs"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.logging.log4j:log4j-api:2.14.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.14.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(16)
+}
+
+application {
+    mainClass.set("MainKt")
+}
